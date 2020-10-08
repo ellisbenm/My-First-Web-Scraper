01716c6dcc3b230bfe14d177efee8878eee96353
ellisbenm <38117683+ellisbenm@users.noreply.github.com>
2020-10-08 09:29:45 -0700

    first push

---
+++ Scraper Script 1 paranoid copy.py
@@ -0,0 +1,25 @@
+#First time web scrapin, kinda nervous
+
+# test url:  https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id="ResultsContainer")
+
+job_elems = results.find_all('section', class_='card-content')
+
+for job_elem in job_elems:
+    # Each job_elem is a new BeautifulSoup object.
+    # You can use the same methods on it as you did before.
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    print(title_elem)
+    print(company_elem)
+    print(location_elem)
+    print()
